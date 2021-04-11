1ea3cfe85147e8eb039e859379dc4f3e7b14ac92
nitin.poosarla <nithin.poosarla@gmail.com>
2021-04-11 15:36:51 +0400

    commiting to TestApp Repo

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/data")
+	public String data()
+	{
+		return "hello how r ";
+	}
+	
+}
